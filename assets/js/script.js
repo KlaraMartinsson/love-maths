987e7d4c30ee98e2a28b3a89dcd15039a3a06e63
KlaraMartinsson <>
2023-07-31 08:06:11 +0000

    Add code to check the answer and respond to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 // Wait for the DOM to finish loading, before running the game
 // Get the button elements and add event listeners to them
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
-        button.addEventListener("click", function () {
+        button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -28,18 +28,26 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
-        throw `Unknown game type:${gameType}. Aborting!`;
+        throw `Unknown game type: ${gameType}. Aborting!`;
 
     }
 }
 
 function checkAnswer() {
 
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert("Hey! You got it right! :D");
+    } else {
+        alert(`Wrong answer, you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`)
+    }
 }
-/**
- * Gets the operands (the numbers) and the operator (plus, minus etc)
- * directly from the dom, and returns the correct answer.
- */
+
+runGame(calculatedAnswer[1]);
+
 function calculateCorrectAnswer() {
     let operand1 = parseInt(document.getElementById('operand1').innerText);
     let operand2 = parseInt(document.getElementById('operand2').innerText);
@@ -48,8 +56,8 @@ function calculateCorrectAnswer() {
     if(operator === "+"){
         return[operand1 + operand2, "addition"];
     } else {
-        alert(`Unimplemented operator ${operator}`);
-        throw `Unimplemented operator ${operator}. Aborting!`;
+        alert(`Unimplemented operator ${ operator }`);
+        throw `Unimplemented operator ${ operator }.Aborting!`;
     }
 }
 
