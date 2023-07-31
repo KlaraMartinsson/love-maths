91cae852438b676aa2075eaf871364ea68c1039b
KlaraMartinsson <>
2023-07-31 17:01:28 +0000

    Add favicon and tidy ups

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,12 +14,18 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     }
+    document.getElementById("answer-box").addEventListener("keydown", function(event){
+if (event.key === "Enter"){
+checkAnswer();
+}
+    })
     runGame("addition");
 });
 
 
 function runGame(gameType) {
-
+document.getElementById("answer-box").value = "";
+document.getElementById("answer-box").focus();
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
@@ -51,9 +57,8 @@ function checkAnswer() {
         alert(`Wrong answer, you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`)
         incrementWrongAnswer();
     }
+    runGame(calculatedAnswer[1]);
 }
-
-runGame(calculatedAnswer[1]);
 
 function calculateCorrectAnswer() {
     let operand1 = parseInt(document.getElementById('operand1').innerText);
@@ -96,8 +101,8 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1;
-    document.getElementById('operand2').textContent = operand1 > operand2 ? operand1;
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand1 : operand2;
     document.getElementById('operator').textContent = "-";
 }
 
